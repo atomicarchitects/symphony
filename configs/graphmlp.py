7646fb743fdfca4ff0d382135db2807b54c1c4cb
Ameya Daigavane <ameya.d.98@gmail.com>
2023-02-14 21:56:16 -0500

    Padding graphs handled in loss.

--- configs/graphmlp.py
+++ configs/graphmlp.py
@@ -4,28 +4,28 @@ import ml_collections
 
 
 def get_config():
-  """Get the hyperparameter configuration for the GraphNetwork model."""
-  config = ml_collections.ConfigDict()
+    """Get the hyperparameter configuration for the GraphNetwork model."""
+    config = ml_collections.ConfigDict()
 
-  # Optimizer.
-  config.optimizer = 'adam'
-  config.learning_rate = 1e-3
+    # Optimizer.
+    config.optimizer = "adam"
+    config.learning_rate = 1e-3
 
-  # Training hyperparameters.
-  config.batch_size = 256
-  config.num_train_steps = 100_000
-  config.log_every_steps = 100
-  config.eval_every_steps = 10_000
-  config.checkpoint_every_steps = 10_000
-  config.add_virtual_node = True
-  config.add_undirected_edges = True
-  config.add_self_loops = True
+    # Training hyperparameters.
+    config.batch_size = 256
+    config.num_train_steps = 100_000
+    config.log_every_steps = 100
+    config.eval_every_steps = 10_000
+    config.checkpoint_every_steps = 10_000
+    config.add_virtual_node = True
+    config.add_undirected_edges = True
+    config.add_self_loops = True
 
-  # GNN hyperparameters.
-  config.model = 'GraphMLP'
-  config.latent_size = 256
-  config.dropout_rate = 0.1
-  config.num_mlp_layers = 3
-  config.output_nodes_size = 128
-  config.layer_norm = True
-  return config
+    # GNN hyperparameters.
+    config.model = "GraphMLP"
+    config.latent_size = 256
+    config.dropout_rate = 0.1
+    config.num_mlp_layers = 3
+    config.output_nodes_size = 128
+    config.layer_norm = True
+    return config
