499a46c155b48e2a7088856e55101840e60e615b
Mario Geiger <geiger.mario@gmail.com>
2023-02-19 10:09:15 -0500

    add root_dir option

--- dispatch_fragmenter.py
+++ dispatch_fragmenter.py
@@ -6,14 +6,14 @@ import time
 from qm9 import load_qm9
 
 
-def main(chunk: int = 3000, num_seeds: int = 8):
+def main(chunk: int = 3000, num_seeds: int = 8, root_dir: str = "data"):
     qm9 = load_qm9("qm9_data")
     processes = []
 
     for seed in range(num_seeds):
         for start in range(0, len(qm9), chunk):
             end = start + chunk
-            path = f"data/fragments_{seed}_{start}_{end}.pkl"
+            path = f"{root_dir}/fragments_{seed}_{start}_{end}.pkl"
 
             if os.path.exists(path):
                 print(f"Skip {path}")
@@ -55,5 +55,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--chunk", type=int, default=3000)
     parser.add_argument("--num_seeds", type=int, default=8)
+    parser.add_argument("--root_dir", type=str, default="data")
     args = parser.parse_args()
     main(**vars(args))
