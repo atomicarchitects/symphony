8ecd0684dffb719f22affa649fa673289abcf041
Mario Geiger <geiger.mario@gmail.com>
2023-02-26 11:41:31 -0500

    black

--- generate_data/fragmenter.py
+++ generate_data/fragmenter.py
@@ -53,7 +53,9 @@ def main(seed: int, start: int, end: int, output: str):
         "stop": tf.TensorSpec(shape=(1,), dtype=tf.bool),
         "target_positions": tf.TensorSpec(shape=(1, 3), dtype=tf.float32),
         "target_species": tf.TensorSpec(shape=(1,), dtype=tf.int32),
-        "target_species_probability": tf.TensorSpec(shape=(1, len(atomic_numbers)), dtype=tf.float32),
+        "target_species_probability": tf.TensorSpec(
+            shape=(1, len(atomic_numbers)), dtype=tf.float32
+        ),
         # n_node and n_edge
         "n_node": tf.TensorSpec(shape=(1,), dtype=tf.int32),
         "n_edge": tf.TensorSpec(shape=(1,), dtype=tf.int32),
@@ -69,7 +71,9 @@ def main(seed: int, start: int, end: int, output: str):
             for frag in frags:
                 d = np.linalg.norm(frag.globals.target_positions)
                 if d > filtering_threshold:
-                    print("Target position is too far away from the rest of the molecule.")
+                    print(
+                        "Target position is too far away from the rest of the molecule."
+                    )
                     skip = True
 
             if skip:
@@ -79,13 +83,19 @@ def main(seed: int, start: int, end: int, output: str):
                 yield {
                     "positions": frag.nodes.positions.astype(np.float32),
                     "species": frag.nodes.species.astype(np.int32),
-                    "focus_probability": frag.nodes.focus_probability.astype(np.float32),
+                    "focus_probability": frag.nodes.focus_probability.astype(
+                        np.float32
+                    ),
                     "senders": frag.senders.astype(np.int32),
                     "receivers": frag.receivers.astype(np.int32),
                     "stop": frag.globals.stop.astype(np.bool_),
-                    "target_positions": frag.globals.target_positions.astype(np.float32),
+                    "target_positions": frag.globals.target_positions.astype(
+                        np.float32
+                    ),
                     "target_species": frag.globals.target_species.astype(np.int32),
-                    "target_species_probability": frag.globals.target_species_probability.astype(np.float32),
+                    "target_species_probability": frag.globals.target_species_probability.astype(
+                        np.float32
+                    ),
                     "n_node": frag.n_node.astype(np.int32),
                     "n_edge": frag.n_edge.astype(np.int32),
                 }
