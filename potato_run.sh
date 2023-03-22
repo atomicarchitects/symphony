16ea1a268242b0aa6e8bbff129d0d69940772d30
Ameya Daigavane <ameya.d.98@gmail.com>
2023-03-21 22:47:48 -0400

    Setup next training run.

--- potato_run.sh
+++ potato_run.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-for l in 0 1 2 3 4 5
+for l in 1
 do
-   for c in 32 64
+   for c in 32
    do
-      for i in 1 2 3 4
+      for i in 1
       do
-        grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v2/mace/interactions="$i"/l="$l"/channels="$c"/
-        grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v2/e3schnet/interactions="$i"/l="$l"/channels="$c"/
+        grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/mace/interactions="$i"/l="$l"/channels="$c"/ &
+        grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/e3schnet/interactions="$i"/l="$l"/channels="$c"/ &
+        grun python -m main --config=configs/nequip.py   --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/nequip/interactions="$i"/l="$l"/channels="$c"/ &
       done
     done
 done
+
+
+# for l in 0 1 2 3 4 5
+# do
+#    for c in 32 64
+#    do
+#       for i in 1 2 3 4
+#       do
+#         grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/mace/interactions="$i"/l="$l"/channels="$c"/ &
+#         grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/e3schnet/interactions="$i"/l="$l"/channels="$c"/ &
+#         grun python -m main --config=configs/nequip.py   --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/nequip/interactions="$i"/l="$l"/channels="$c"/ &
+#       done
+#     done
+# done
