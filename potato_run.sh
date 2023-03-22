9f8ec78248ae0ed011b4a3bbcd27de69efc62e71
Ameya Daigavane <ameya.d.98@gmail.com>
2023-03-21 23:51:04 -0400

    Setup training run v3.

--- potato_run.sh
+++ potato_run.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
 
-for l in 1
+for l in 0 1 2 3
 do
-   for c in 32
-   do
-      for i in 1
-      do
-        grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/exps/mace/interactions="$i"/l="$l"/channels="$c"/ 2> /tmp/exps/out.txt &
+  for c in 32
+  do
+    for i in 1 2 3 4
+    do
+        grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/mace/interactions="$i"/l="$l"/channels="$c"/     2> workdirs/v3/out.txt &
         sleep 10
-        grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/exps/e3schnet/interactions="$i"/l="$l"/channels="$c"/ 2> /tmp/exps/out.txt &
+        grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/e3schnet/interactions="$i"/l="$l"/channels="$c"/ 2> workdirs/v3/out.txt &
         sleep 10
-        grun python -m main --config=configs/nequip.py   --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=/tmp/exps/nequip/interactions="$i"/l="$l"/channels="$c"/ 2> /tmp/exps/out.txt &
+        grun python -m main --config=configs/nequip.py   --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/nequip/interactions="$i"/l="$l"/channels="$c"/   2> workdirs/v3/out.txt &
         sleep 10
-      done
     done
+  done
 done
 
-
-# for l in 0 1 2 3 4 5
-# do
-#    for c in 32 64
-#    do
-#       for i in 1 2 3 4
-#       do
-#         grun python -m main --config=configs/mace.py     --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/mace/interactions="$i"/l="$l"/channels="$c"/ &
-#         grun python -m main --config=configs/e3schnet.py --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/e3schnet/interactions="$i"/l="$l"/channels="$c"/ &
-#         grun python -m main --config=configs/nequip.py   --config.max_ell="$l" --config.num_channels="$c" --config.num_interactions="$i" --workdir=workdirs/v3/nequip/interactions="$i"/l="$l"/channels="$c"/ &
-#       done
-#     done
-# done
