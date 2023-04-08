bf32c33850378033bfeb5b09c2ac472d1be80d07
Ameya Daigavane <ameya.d.98@gmail.com>
2023-04-08 18:10:38 -0400

    Improve 3D visualizations.

--- analyses/visualize_atom_removals.py
+++ analyses/visualize_atom_removals.py
@@ -70,6 +70,15 @@ def visualize_atom_removals(
         preds = apply_fn(params, dummy_rng, frags, beta)
         pred = jraph.unpad_with_graphs(preds)
         pred = jax.tree_map(np.asarray, pred)
+        pred = datatypes.Predictions(
+            globals=jax.tree_map(lambda x: x.squeeze(axis=0), pred.globals),
+            nodes=pred.nodes,
+            edges=pred.edges,
+            senders=pred.senders,
+            receivers=pred.receivers,
+            n_node=pred.n_node,
+            n_edge=pred.n_edge,
+        )
         return pred
 
     def remove_atom_and_visualize_predictions(
@@ -92,34 +101,34 @@ def visualize_atom_removals(
         pred = get_predictions(frag)
 
         # Compute focus probabilities.
-        num_nodes = len(molecule)
-        stop_probs = pred.globals.stop_probs.item()
         focus = pred.globals.focus_indices.item()
         focus_position = frag.nodes.positions[focus]
-        focus_probs_shifted = np.concatenate([pred.nodes.focus_probs[:target], [0], pred.nodes.focus_probs[target:]])
-        focus_probs_renormalized = focus_probs_shifted * (1 - stop_probs)
+        stop_prob = pred.globals.stop_probs.item()
+        focus_probs = pred.nodes.focus_probs
+        focus_probs = (1 - stop_prob) * focus_probs
 
         # Plot species probabilities.
         target_element_index = ATOMIC_NUMBERS.index(molecule.numbers[target])
-        species_probs = pred.globals.target_species_probs[0].tolist()
-        species_fig = go.Figure(
-            [
-                go.Bar(
-                    x=[ELEMENTS[target_element_index]],
-                    y=[species_probs[target_element_index]],
-                ),
-                go.Bar(
-                    x=ELEMENTS[:target_element_index]
-                    + ELEMENTS[target_element_index + 1 :],
-                    y=species_probs[:target_element_index]
-                    + species_probs[target_element_index + 1 :],
-                ),
-            ]
+        species_probs = pred.globals.target_species_probs.tolist()
+        species_trace = [
+            go.Bar(
+                x=[ELEMENTS[target_element_index]],
+                y=[species_probs[target_element_index]],
+            ),
+            go.Bar(
+                x=ELEMENTS[:target_element_index]
+                + ELEMENTS[target_element_index + 1 :],
+                y=species_probs[:target_element_index]
+                + species_probs[target_element_index + 1 :],
+            ),
+        ]
+
+        species_fig = go.Figure(species_trace
         )
 
         # Plot the actual molecule.
-        mol_fig_data = []
-        mol_fig_data.append(
+        mol_trace = []
+        mol_trace.append(
             go.Scatter3d(
                 x=molecule.positions[:, 0],
                 y=molecule.positions[:, 1],
@@ -130,36 +139,41 @@ def visualize_atom_removals(
                     color=[ATOMIC_COLORS[i] for i in molecule.numbers],
                 ),
                 hovertext=[ase.data.chemical_symbols[i] for i in molecule.numbers],
-                text=["p(focus) = {:.2f}".format(focus_probs_renormalized[i]) if i != target else "Removed" for i in range(num_nodes)],
                 opacity=1.0,
                 name="Molecule",
             )
         )
-        # Highlight the focus atom.
-        mol_fig_data.append(
+        # Highlight the focus probabilities.
+        print(focus_probs)
+        def get_size(x):
+            if x < 0.1:
+                return 0
+            return 1 + x
+
+        mol_trace.append(
             go.Scatter3d(
-                x=[molecule_with_target_removed.positions[focus, 0]],
-                y=[molecule_with_target_removed.positions[focus, 1]],
-                z=[molecule_with_target_removed.positions[focus, 2]],
+                x=molecule_with_target_removed.positions[:, 0],
+                y=molecule_with_target_removed.positions[:, 1],
+                z=molecule_with_target_removed.positions[:, 2],
                 mode="markers",
                 marker=dict(
-                    size=1.05 * ATOMIC_SIZES[molecule_with_target_removed.numbers[focus]],
-                    color="yellow",
+                    size=[get_size(focus_probs[i]) * ATOMIC_SIZES[i] for i in molecule_with_target_removed.numbers],
+                    color=["yellow" for _ in molecule_with_target_removed.numbers],
                 ),
                 opacity=0.5,
-                name="Focus",
+                name="Focus Probabilities",
             )   
         )
         # Highlight the target atom.
-        mol_fig_data.append(
+        mol_trace.append(
             go.Scatter3d(
                 x=[molecule.positions[target, 0]],
                 y=[molecule.positions[target, 1]],
                 z=[molecule.positions[target, 2]],
                 mode="markers",
                 marker=dict(
-                    size=1.05 * ATOMIC_SIZES[molecule.numbers[target]],
-                    color="green",
+                    size=[1.05 * ATOMIC_SIZES[molecule.numbers[target]]],
+                    color=["green"],
                 ),
                 opacity=0.5,
                 name="Target",
@@ -167,8 +181,7 @@ def visualize_atom_removals(
         )
 
         target_sp = pred.globals.target_species.item()
-        target_color = ATOMIC_COLORS[ATOMIC_NUMBERS[target_sp]]
-        target_logits = e3nn.to_s2grid(
+        position_logits = e3nn.to_s2grid(
             pred.globals.position_coeffs,
             50,
             99,
@@ -177,30 +190,26 @@ def visualize_atom_removals(
             p_val=1,
             p_arg=-1,
         )
-        target_probs = target_logits.apply(
-            lambda x: jnp.exp(x - target_logits.grid_values.max())
+        position_probs = position_logits.apply(
+            lambda x: jnp.exp(x - position_logits.grid_values.max())
         )
-
-        cmin = 0.0
-        cmax = target_probs.grid_values.max().item()
+        cmin = 0.1
+        cmax = position_probs.grid_values.max().item()
         for i in range(len(RADII)):
-            prob_r = target_probs[0, i]
-
-            if prob_r.grid_values.max() < cmax / 100.0:
-                continue
-
+            prob_r = position_probs[i]
             surface_r = go.Surface(
                 **prob_r.plotly_surface(radius=RADII[i], translation=focus_position),
                 colorscale=[
-                    [0, f"rgba({target_color[4:-1]}, 0.0)"],
-                    [1, f"rgba({target_color[4:-1]}, 1.0)"],
+                    [0, f"rgba(0, 0, 0, 0.0)"],
+                    [1, f"rgba(0, 0, 0, 1.0)"],
                 ],
                 showscale=False,
                 cmin=cmin,
                 cmax=cmax,
-                name=f"Prediction: {ase.data.chemical_symbols[ATOMIC_NUMBERS[target_sp]]}",
+                name=f"Position Probabilities",
+                showlegend=(i == 0),
             )
-            mol_fig_data.append(surface_r)
+            mol_trace.append(surface_r)
 
         axis = dict(
             showbackground=False,
@@ -212,8 +221,8 @@ def visualize_atom_removals(
         )
 
         layout = go.Layout(
-            width=1200,
-            height=800,
+            width=800,
+            height=600,
             scene=dict(
                 xaxis=dict(**axis),
                 yaxis=dict(**axis),
@@ -230,9 +239,15 @@ def visualize_atom_removals(
             plot_bgcolor="rgba(0,0,0,0)",
             margin=dict(l=0, r=0, t=0, b=0),
         )
-        mol_fig = go.Figure(data=mol_fig_data, layout=layout)
+        mol_fig = go.Figure(data=mol_trace, layout=layout)
+        mol_fig.update_layout(legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ))
 
-        return focus_fig, species_fig, mol_fig
+        return species_fig, mol_fig
 
     # Create the output directory where HTML files will be saved.
     step_name = "step=best" if step == -1 else f"step={step}"
@@ -248,16 +263,11 @@ def visualize_atom_removals(
     os.makedirs(outputdir, exist_ok=True)
 
     # Loop over all possible targets.
-    for target in tqdm.tqdm(range(len(molecule)), desc="Targets"):
-        focus_fig, species_fig, mol_fig = remove_atom_and_visualize_predictions(
+    # for target in tqdm.tqdm(range(len(molecule)), desc="Targets"):
+    for target in tqdm.tqdm(range(1), desc="Targets"):
+        species_fig, mol_fig = remove_atom_and_visualize_predictions(
             molecule, target=target
         )
-        outputfile = os.path.join(
-            outputdir,
-            f"{molecule_name}_target={target}_focus.png",
-        )
-        focus_fig.write_image(outputfile)
-
         outputfile = os.path.join(
             outputdir,
             f"{molecule_name}_target={target}_species.png",
