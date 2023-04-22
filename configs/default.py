04f4e74c5acc7d8d9d8d0d2516ac0d80817876ec
Mario Geiger <geiger.mario@gmail.com>
2023-04-22 14:37:29 -0400

    alpha param

--- configs/default.py
+++ configs/default.py
@@ -53,7 +53,7 @@ def get_config() -> ml_collections.ConfigDict:
     config.nn_tolerance = 0.5
     config.nn_cutoff = 5.0
     config.compute_padding_dynamically = False
-    config.max_n_graphs = 8
+    config.max_n_graphs = 32
     config.max_n_nodes = 30 * config.get_ref("max_n_graphs")
     config.max_n_edges = 90 * config.get_ref("max_n_graphs")
     config.loss_kwargs = ml_collections.ConfigDict()
