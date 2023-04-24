516df5ee3ca7ba281b5c97157c5f6827524f883a
Ameya Daigavane <ameya.d.98@gmail.com>
2023-04-24 12:55:57 -0400

    Make position distributions more diffuse.

--- configs/default.py
+++ configs/default.py
@@ -58,8 +58,8 @@ def get_config() -> ml_collections.ConfigDict:
     config.max_n_nodes = 30 * config.get_ref("max_n_graphs")
     config.max_n_edges = 90 * config.get_ref("max_n_graphs")
     config.loss_kwargs = ml_collections.ConfigDict()
-    config.loss_kwargs.radius_rbf_variance = 1e-3
-    config.loss_kwargs.target_position_scaling_constant = 1e3
+    config.loss_kwargs.radius_rbf_variance = 5e-2
+    config.loss_kwargs.target_position_scaling_constant = 5e1
 
     # Prediction heads.
     config.focus_predictor = ml_collections.ConfigDict()
