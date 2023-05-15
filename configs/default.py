78badcd44a82cb5d6a14f3dc185a9a9af871ec02
Ameya Daigavane <ameya.d.98@gmail.com>
2023-05-15 10:12:43 -0400

    Fix default inverse temperature.

--- configs/default.py
+++ configs/default.py
@@ -59,7 +59,7 @@ def get_config() -> ml_collections.ConfigDict:
     config.max_n_edges = 90 * config.get_ref("max_n_graphs")
     config.loss_kwargs = ml_collections.ConfigDict()
     config.loss_kwargs.radius_rbf_variance = 1e-3
-    config.loss_kwargs.target_position_inverse_temperature = 1e3
+    config.loss_kwargs.target_position_inverse_temperature = 1.
     config.loss_kwargs.ignore_position_loss_for_small_fragments = False
     config.loss_kwargs.position_loss_type = "kl_divergence"
 
