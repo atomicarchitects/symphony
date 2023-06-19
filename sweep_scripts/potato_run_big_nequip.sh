278e31dbdc73b7b53e4f025cc69778fbae51f704
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-19 19:54:38 -0400

    Fix sweep script.

--- sweep_scripts/potato_run_big_nequip.sh
+++ sweep_scripts/potato_run_big_nequip.sh
@@ -4,13 +4,16 @@
 expname=v7
 
 # Loop over hyperparameters
-for l in 5
+for model in "nequip"
 do
-  for c in 32
+  for l in 1 2 3 4 5
   do
-    for i in 4
+    for c in 32
     do
-        CUDA_VISIBLE_DEVICES=4 python -m main --config=configs/nequip.py --config.max_n_graphs=32  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.loss_kwargs.position_loss_type="l2" --config.num_train_steps=3200000 --config.loss_kwargs.target_position_inverse_temperature=1 --workdir=workdirs/"$expname"/nequip-l2/interactions="$i"/l="$l"/channels="$c"/  || break 10
+      for i in 4
+      do
+          CUDA_VISIBLE_DEVICES="$l" python -m main --config=configs/"$model".py --config.max_n_graphs=32  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.num_train_steps=1000000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_c="$c"_i="$i".txt 2>&1  &
+      done
     done
   done
 done
