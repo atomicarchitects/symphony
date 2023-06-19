197c51a6eb52cf7ccc7efd14f3cf32e6a198744c
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-19 19:29:08 -0400

    Format with black.

--- analyses/generate_molecules.py
+++ analyses/generate_molecules.py
@@ -277,8 +277,9 @@ def generate_molecules(
         for step in range(MAX_NUM_ATOMS):
             step_rng, rng = jax.random.split(rng)
             fragment = input_pipeline.ase_atoms_to_jraph_graph(
-                molecule, models.ATOMIC_NUMBERS, config.nn_cutoff)
-            
+                molecule, models.ATOMIC_NUMBERS, config.nn_cutoff
+            )
+
             # Run the model on the current molecule.
             pred = get_predictions(fragment, step_rng)
 
