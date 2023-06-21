1729778ef7f3b95844d6a4abd3f1e27fbc816625
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-21 12:31:41 -0400

    Add tetris sweep.

--- sweep_scripts/potato_run_big_nequip.sh
+++ sweep_scripts/potato_run_big_nequip.sh
@@ -6,13 +6,13 @@ expname=v7
 # Loop over hyperparameters
 for model in "nequip"
 do
-  for l in 1 2 3 4 5
+  for l in 1
   do
-    for c in 32
+    for c in 64
     do
-      for i in 4
+      for i in 1 2 3 4 5 6
       do
-          CUDA_VISIBLE_DEVICES="$l" python -m main --config=configs/"$model".py --config.max_n_graphs=32  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.num_train_steps=1000000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_c="$c"_i="$i".txt 2>&1  &
+          CUDA_VISIBLE_DEVICES="$((i-1))" python -m main --config=configs/"$model".py --config.max_n_graphs=32  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.num_train_steps=200000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_c="$c"_i="$i".txt 2>&1  &
       done
     done
   done
