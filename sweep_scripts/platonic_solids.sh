7704832f4b5f36d95e0109d8a669d25b035e838c
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-28 00:20:49 -0400

    Add script for platonic solids.

---
+++ sweep_scripts/platonic_solids.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set experiment name
+expname=platonic_solids
+
+# Loop over hyperparameters
+for model in "nequip"
+do
+  for l in 1 2 3 4 5
+  do
+    for c in 64
+    do
+      for i in 2
+      do
+          CUDA_VISIBLE_DEVICES="$((l-1))" python -m main --config=configs/platonic_solids/"$model".py --config.dataset="platonic_solids"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_c="$c"_i="$i".txt 2>&1  &
+      done
+    done
+  done
+done
+
+
