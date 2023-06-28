183c0c99efff295426e59c3041ac2832c2bd778b
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-28 00:26:59 -0400

    Fix workdir.

--- sweep_scripts/platonic_solids.sh
+++ sweep_scripts/platonic_solids.sh
@@ -14,7 +14,7 @@ do
       do
         for i in 2
         do
-            CUDA_VISIBLE_DEVICES="$((pc-1))" python -m main --config=configs/platonic_solids/"$model".py --config.dataset="platonic_solids"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c" --config.target_position_predictor.num_channels="$pc"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
+            CUDA_VISIBLE_DEVICES="$((pc-1))" python -m main --config=configs/platonic_solids/"$model".py --config.dataset="platonic_solids"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c" --config.target_position_predictor.num_channels="$pc"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"/  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
         done
       done
     done
