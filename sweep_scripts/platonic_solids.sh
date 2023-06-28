a47f61b83f7a05eb3f7eb0c8db0307325816e6bb
Ameya Daigavane <ameya.d.98@gmail.com>
2023-06-28 00:24:40 -0400

    Add position channels.

--- sweep_scripts/platonic_solids.sh
+++ sweep_scripts/platonic_solids.sh
@@ -6,13 +6,16 @@ expname=platonic_solids
 # Loop over hyperparameters
 for model in "nequip"
 do
-  for l in 1 2 3 4 5
+  for l in 1
   do
-    for c in 64
+    for pc in 1 2 5
     do
-      for i in 2
+      for c in 64
       do
-          CUDA_VISIBLE_DEVICES="$((l-1))" python -m main --config=configs/platonic_solids/"$model".py --config.dataset="platonic_solids"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_c="$c"_i="$i".txt 2>&1  &
+        for i in 2
+        do
+            CUDA_VISIBLE_DEVICES="$((pc-1))" python -m main --config=configs/platonic_solids/"$model".py --config.dataset="platonic_solids"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c" --config.target_position_predictor.num_channels="$pc"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/channels="$c"/  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
+        done
       done
     done
   done
