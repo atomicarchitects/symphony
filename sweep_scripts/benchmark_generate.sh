da8baf6d83dcff8bbf9f34253e6b01ab5a3d0a14
Ameya Daigavane <ameya.d.98@gmail.com>
2023-07-17 14:44:47 -0400

    Commit latest sweeps.

--- sweep_scripts/benchmark_generate.sh
+++ sweep_scripts/benchmark_generate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-for max_num_atoms in 5 10 20
-do 
+for max_num_atoms in 5 10 20 30
+do
     for num_seeds in 25 100 500 1000 5000 10000
     do
-        time CUDA_VISIBLE_DEVICES=0 python -m analyses.generate_molecules --workdir=workdirs/qm9_10JUL/nequip/interactions=2/l=4/position_channels=5/channels=64/ --fait=1 --pit=1 --init=H --max_num_atoms="$max_num_atoms" --num_seeds="$num_seeds" --num_seeds_per_chunk=25 > benchmark_generate_"$max_num_atoms"_"$num_seeds".txt 2>&1
+        CUDA_VISIBLE_DEVICES=0 time python -m analyses.generate_molecules_batched --workdir=workdirs/qm9_10JUL/nequip/interactions=2/l=4/position_channels=5/channels=64/ --fait=1 --pit=1 --init=H --max_num_atoms="$max_num_atoms" --num_seeds="$num_seeds" --num_seeds_per_chunk=25 > benchmark_generate_batched/benchmark_generate_batched_atoms="$max_num_atoms"_seeds="$num_seeds".txt 2>&1
     done
 done
--- sweep_scripts/qm9.sh
+++ sweep_scripts/qm9.sh
@@ -13,9 +13,9 @@ do
     do
       for c in 64
       do
-        for i in 2
-	    do
-            CUDA_VISIBLE_DEVICES="$((l))" python -m symphony --config=configs/"$dataset"/"$model".py --config.dataset="$dataset"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c" --config.target_position_predictor.num_channels="$pc"  --config.num_interactions="$i" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
+        for i in 3
+	do
+            CUDA_VISIBLE_DEVICES="$((l-1))" python -m symphony --config=configs/"$dataset"/"$model".py --config.dataset="$dataset"  --config.max_n_graphs=16  --config.max_ell="$l" --config.num_channels="$c" --config.target_position_predictor.num_channels="$pc"  --config.num_interactions="$i" --config.num_train_steps=400000 --config.compute_global_embedding=False --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
         done
       done
     done
