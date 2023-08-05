056e76b7f62add7e3e7236069036e2c957c95a28
Ameya Daigavane <ameya.d.98@gmail.com>
2023-08-05 00:15:41 -0400

    Save sweep scripts as of August 4th.

--- sweep_scripts/generate_molecules.sh
+++ sweep_scripts/generate_molecules.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-workdir="qm9_15JUL"
+workdir="qm9_bessel_embedding"
 
-for l in 1 2 3 4
+for l in 1 2 3 4 5
 do
-    for i in 3
+  for i in 3
+  do
+    for c in 64
     do
       for pc in 2
       do
-          CUDA_VISIBLE_DEVICES=6 python -m analyses.generate_molecules  --workdir=workdirs/"$workdir"/nequip/interactions="$i"/l="$l"/position_channels="$pc"/channels=64/  --fait=1 --pit=1 --init=H  --max_num_atoms=30 --num_seeds=1000 --num_seeds_per_chunk=25
+        for step in 100000 200000 500000 1000000 2000000 5000000
+        do
+            CUDA_VISIBLE_DEVICES=1 python -m analyses.generate_molecules  --workdir=workdirs/"$workdir"/nequip/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"/global_embed=False  --fait=1 --pit=1 --init=H  --step="$step"  --max_num_atoms=35 --num_seeds=1000 --num_seeds_per_chunk=25
+        done
       done
     done
+  done
 done
