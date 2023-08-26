24c9c5bf8ea129513df4fb496d944d17d44c6ad9
Ameya Daigavane <ameya.d.98@gmail.com>
2023-08-25 23:28:56 -0400

    Make radius distribution a bit more smooth.

--- configs/qm9/default.py
+++ configs/qm9/default.py
@@ -43,7 +43,7 @@ def get_config() -> ml_collections.ConfigDict:
     config.max_n_nodes = 30 * config.get_ref("max_n_graphs")
     config.max_n_edges = 90 * config.get_ref("max_n_graphs")
     config.loss_kwargs = ml_collections.ConfigDict()
-    config.loss_kwargs.radius_rbf_variance = 1e-3
+    config.loss_kwargs.radius_rbf_variance = 1e-2
     config.loss_kwargs.target_position_inverse_temperature = 20.0
     config.loss_kwargs.target_position_lmax = 5
     config.loss_kwargs.ignore_position_loss_for_small_fragments = False
