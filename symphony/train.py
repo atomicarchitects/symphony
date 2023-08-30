f12b3a37a067025a5b004d2bb02e05c98a76e0c0
Ameya Daigavane <ameya.d.98@gmail.com>
2023-08-30 13:35:11 -0400

    Try training again.

--- symphony/train.py
+++ symphony/train.py
@@ -103,11 +103,11 @@ def train_step(
         _,
         (total_loss, focus_and_atom_type_loss, position_loss, mask),
     ), grads = grad_fn(state.params, graphs)
-    # Log norms of gradients.
-    grad_norms = sum(jax.tree_leaves(jax.tree_map(jnp.linalg.norm, grads)))
-    # jax.debug.print("grad_norms={grad_norms}", grad_norms=grad_norms)        
     state = state.apply_gradients(grads=grads)
 
+    # Log norms of gradients.
+    # grad_norms = sum(jax.tree_leaves(jax.tree_map(jnp.linalg.norm, grads)))
+    # jax.debug.print("grad_norms={grad_norms}", grad_norms=grad_norms)        
     batch_metrics = Metrics.single_from_model_output(
         total_loss=total_loss,
         focus_and_atom_type_loss=focus_and_atom_type_loss,
