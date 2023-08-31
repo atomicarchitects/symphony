3d997f60eaef2be2f5e7412079857c788a5441f8
Ameya Daigavane <ameya.d.98@gmail.com>
2023-08-31 12:50:33 -0400

    Add position denoiser to config.

--- configs/qm9/default.py
+++ configs/qm9/default.py
@@ -78,4 +78,5 @@ def get_config() -> ml_collections.ConfigDict:
     config.target_position_predictor.radial_mlp_num_layers = 2
     config.target_position_predictor.radial_mlp_activation = "swish"
 
+    config.position_denoiser = ml_collections.ConfigDict()
     return config
