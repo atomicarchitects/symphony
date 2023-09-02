eee1e05cc9db79b1e145d98121ed3f4eba221e91
Ameya Daigavane <ameya.d.98@gmail.com>
2023-09-02 00:42:21 -0400

    Only denoising.

--- symphony/loss.py
+++ symphony/loss.py
@@ -460,8 +460,8 @@ def generation_loss(
     loss_denoising = (graphs.globals.stop) * denoising_loss()
 
     # COMMENT LATER.
-    # loss_position = jnp.zeros_like(loss_position)
-    # loss_focus_and_atom_type = jnp.zeros_like(loss_focus_and_atom_type)
+    loss_position = jnp.zeros_like(loss_position)
+    loss_focus_and_atom_type = jnp.zeros_like(loss_focus_and_atom_type)
     # loss_denoising = jnp.zeros_like(loss_denoising)
 
     # Mask out the loss for atom types?
