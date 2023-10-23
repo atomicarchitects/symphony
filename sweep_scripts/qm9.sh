6d4d7e1839bb18686c256fc4b1c0eac7453c54f5
Ameya Daigavane <ameya.d.98@gmail.com>
2023-10-23 11:21:52 -0400

    Increase number of steps.

--- sweep_scripts/qm9.sh
+++ sweep_scripts/qm9.sh
@@ -2,7 +2,7 @@
 
 # Set experiment name
 dataset="qm9"
-expname="$dataset"_bessel_embedding_attempt7_edm_splits_lower_lr
+expname="$dataset"_bessel_embedding_attempt6_edm_splits
 
 # Loop over hyperparameters
 for model in "e3schnet_and_nequip"
@@ -17,7 +17,7 @@ do
       do
         for i in 3
 	do
-           CUDA_VISIBLE_DEVICES=0,1 python -m symphony \
+           CUDA_VISIBLE_DEVICES=4,5 python -m symphony \
            --config=configs/"$dataset"/"$model".py --config.dataset="$dataset" \
            --config.focus_and_target_species_predictor.embedder_config.max_ell="$lfocus" \
            --config.target_position_predictor.embedder_config.max_ell="$l" \
@@ -26,10 +26,12 @@ do
            --config.focus_and_target_species_predictor.embedder_config.num_interactions="$i" \
            --config.target_position_predictor.embedder_config.num_interactions="$i" \
            --config.target_position_predictor.num_channels="$pc" \
-           --config.num_train_steps=10000000 \
+           --config.num_train_steps=40000000 \
+           --config.target_position_predictor.res_beta=45 \
+           --config.target_position_predictor.res_alpha=89 \
            --config.position_noise_std=0.05 \
-           --config.max_n_graphs=8 \
-           --config.learning_rate=0.0002 \
+           --config.max_n_graphs=16 \
+           --config.learning_rate=0.0005 \
            --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"/  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i".txt 2>&1  &
         done
       done
