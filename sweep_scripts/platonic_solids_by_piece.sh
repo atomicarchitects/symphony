78a77ec05c68e34603a98be8a34b0b55ad8abb84
Ameya Daigavane <ameya.d.98@gmail.com>
2024-01-10 09:27:59 -0500

    Sweep script that gets low loss (not sure about generations).

--- sweep_scripts/platonic_solids_by_piece.sh
+++ sweep_scripts/platonic_solids_by_piece.sh
@@ -2,27 +2,47 @@
 
 # Set experiment name
 dataset="platonic_solids"
-expname="$dataset"_without_extra_irreps_with_noise
+expname="$dataset"_by_piece_meanangular
 
 # Loop over hyperparameters
-for model in "e3schnet_and_nequip"
+for piece in 0 1 2 3 4
 do
-  for l in 4
+for model in "nequip"
+do
+  for lfocus in 2
   do
-    for pc in 5
+  for l in 2
+  do
+    for pc in 1
     do
-      for c in 64
+      for c in 32
       do
-        for i in 3
-	do
-          for piece in 0 1 2 3 4
-          do
-            CUDA_VISIBLE_DEVICES="$((piece + 2))" python -m symphony --config=configs/"$dataset"/"$model".py --config.add_noise_to_positions=True --config.position_noise_std=0.1  --config.train_pieces="($piece, $((piece + 1)))" --config.val_pieces="($piece, $((piece + 1)))" --config.test_pieces="($piece, $((piece + 1)))" --config.dataset="$dataset"  --config.max_n_graphs=16 --config.target_position_predictor.num_channels="$pc" --config.num_train_steps=100000 --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/position_channels="$pc"/channels="$c"/piece="$piece"  > "$expname"_model="$model"_l="$l"_pc="$pc"_c="$c"_i="$i"_piece="$piece".txt 2>&1  &
+        for i in 2
+        do
+            CUDA_VISIBLE_DEVICES="$piece" python -m symphony \
+            --config=configs/"$dataset"/"$model".py \
+            --config.add_noise_to_positions=True \
+            --config.position_noise_std=0.1 \
+            --config.train_pieces="($piece, $((piece + 1)))" \
+            --config.val_pieces="($piece, $((piece + 1)))" \
+            --config.test_pieces="($piece, $((piece + 1)))" \
+            --config.dataset="$dataset" \
+            --config.max_n_graphs=16 \
+            --config.loss_kwargs.ignore_position_loss_for_small_fragments=False \
+            --config.focus_and_target_species_predictor.embedder_config.max_ell="$lfocus" \
+            --config.target_position_predictor.embedder_config.max_ell="$l" \
+            --config.focus_and_target_species_predictor.embedder_config.num_channels="$c" \
+            --config.target_position_predictor.embedder_config.num_channels="$c" \
+            --config.focus_and_target_species_predictor.embedder_config.num_interactions="$i" \
+            --config.target_position_predictor.embedder_config.num_interactions="$i" \
+            --config.target_position_predictor.num_channels="$pc" \
+            --config.num_train_steps=10000 \
+            --workdir=workdirs/"$expname"/"$model"/interactions="$i"/l="$l"/lfocus="$lfocus"/position_channels="$pc"/channels="$c"/apply_gate="$apply_gate"/square_logits="$square_logits"/piece="$piece" \
+            > "$expname"_model="$model"_l="$l"_lfocus="$lfocus"_pc="$pc"_c="$c"_i="$i"_piece="$piece".txt 2>&1 &
           done
         done
       done
     done
   done
 done
-
-
+done
