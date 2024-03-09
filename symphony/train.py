d6c0ad8e64bc4573d6d3420c91a41fe9e100c7cd
Ameya Daigavane <ameya.d.98@gmail.com>
2024-03-09 17:21:04 -0500

    Remove commented line.

--- symphony/train.py
+++ symphony/train.py
@@ -330,7 +330,6 @@ def train_and_evaluate(
     logging.info("Obtaining datasets.")
     rng = jax.random.PRNGKey(config.rng_seed)
     rng, dataset_rng = jax.random.split(rng)
-    # datasets = input_pipeline.get_datasets(dataset_rng, config)
     datasets = input_pipeline_tf.get_datasets(dataset_rng, config)
 
     # Create and initialize the network.
