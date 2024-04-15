c456a393ef878c8113871975436fc02d2ddd131f
Ameya Daigavane <ameya.d.98@gmail.com>
2024-04-15 18:16:46 -0400

    Fix config.

--- configs/qm9/default.py
+++ configs/qm9/default.py
@@ -37,7 +37,6 @@ def get_config() -> ml_collections.ConfigDict:
     config.rng_seed = 0
     config.num_train_steps = 2_000_000
     config.num_eval_steps = 3000
-    config.num_eval_steps_at_end_of_training = 5000
     config.log_every_steps = 1000
     config.eval_every_steps = 30000
     config.generate_every_steps = 30000
@@ -47,17 +46,9 @@ def get_config() -> ml_collections.ConfigDict:
     config.max_n_nodes = 30 * config.get_ref("max_n_graphs")
     config.max_n_edges = 90 * config.get_ref("max_n_graphs")
     config.loss_kwargs = ml_collections.ConfigDict()
-    config.loss_kwargs.radius_rbf_variance = 1e-5
-    config.loss_kwargs.target_position_inverse_temperature = 20.0
-    config.loss_kwargs.target_position_lmax = 5
     config.loss_kwargs.ignore_position_loss_for_small_fragments = False
-    config.loss_kwargs.position_loss_type = "kl_divergence"
-    config.loss_kwargs.radial_loss_scaling_factor = 1.0
-    config.loss_kwargs.mask_atom_types = False
-    config.mask_atom_types = False
     config.add_noise_to_positions = True
     config.position_noise_std = 0.1
-    config.freeze_node_embedders = False
 
     # Prediction heads.
     config.focus_and_target_species_predictor = ml_collections.ConfigDict()
@@ -67,24 +58,30 @@ def get_config() -> ml_collections.ConfigDict:
     config.focus_and_target_species_predictor.activation = "softplus"
 
     config.target_position_predictor = ml_collections.ConfigDict()
-    config.target_position_predictor.res_beta = 180
-    config.target_position_predictor.res_alpha = 359
-    config.target_position_predictor.num_channels = 5
-    config.target_position_predictor.min_radius = 0.9
-    config.target_position_predictor.max_radius = 2.0
-    config.target_position_predictor.num_radii = 64
-    config.target_position_predictor.apply_gate = False
-    config.target_position_predictor.factorized = False
-    config.target_position_predictor.radial_mlp_latent_size = 128
-    config.target_position_predictor.radial_mlp_num_layers = 2
-    config.target_position_predictor.radial_mlp_activation = "swish"
+    config.target_position_predictor.angular_predictor = ml_collections.ConfigDict()
+    config.target_position_predictor.angular_predictor.num_channels = 2
+    config.target_position_predictor.angular_predictor.radial_mlp_num_layers = 2
+    config.target_position_predictor.angular_predictor.radial_mlp_latent_size = 8
+    config.target_position_predictor.angular_predictor.res_beta = 100
+    config.target_position_predictor.angular_predictor.res_alpha = 99
+    config.target_position_predictor.angular_predictor.quadrature = "gausslegendre"
+    config.target_position_predictor.angular_predictor.sampling_inverse_temperature_factor = 10.0
+    config.target_position_predictor.angular_predictor.sampling_num_steps = 1000
+    config.target_position_predictor.angular_predictor.sampling_init_step_size = 10.0
+
+    config.target_position_predictor.radial_predictor = ml_collections.ConfigDict()
+    config.target_position_predictor.radial_predictor.num_bins = 16
+    config.target_position_predictor.radial_predictor.num_param_mlp_layers = 2
+    config.target_position_predictor.radial_predictor.num_layers = 2
+    config.target_position_predictor.radial_predictor.min_radius = 0.0
+    config.target_position_predictor.radial_predictor.max_radius = 5.0
 
     # Generation.
     config.generation = ml_collections.ConfigDict()
     config.generation.focus_and_atom_type_inverse_temperature = 1.0
     config.generation.position_inverse_temperature = 1.0
-    config.generation.res_beta = config.target_position_predictor.get_ref("res_beta")
-    config.generation.res_alpha = config.target_position_predictor.get_ref("res_alpha")
+    config.generation.res_beta = config.target_position_predictor.angular_predictor.get_ref("res_beta")
+    config.generation.res_alpha = config.target_position_predictor.angular_predictor.get_ref("res_alpha")
     config.generation.radial_cutoff = config.get_ref("radial_cutoff")
     config.generation.num_seeds = 100
     config.generation.num_seeds_per_chunk = 20
