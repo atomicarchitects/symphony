Fix normalization and NaN issues.

symphony/data/fragments.py
@@ -106,16 +106,12 @@ def _make_first_fragment(
         axis=0,
         weights=(graph.nodes.species > 0) if heavy_first else None,
     )
-    distances_com = np.linalg.norm(graph.nodes.positions - com, axis=1)
-    probs_com = jax.nn.softmax(-beta_com * distances_com**2)
     rng, k = jax.random.split(rng)
     if heavy_first and (graph.nodes.species != 0).sum() > 0:
         heavy_indices = np.argwhere(graph.nodes.species != 0).squeeze(-1)
-        first_node = jax.random.choice(k, heavy_indices, p=probs_com[heavy_indices])
+        first_node = jax.random.choice(k, heavy_indices)
     else:
-        first_node = jax.random.choice(
-            k, np.arange(0, len(graph.nodes.positions)), p=probs_com
-        )
+        first_node = jax.random.choice(k, np.arange(0, len(graph.nodes.positions)))
     first_node = int(first_node)
 
     mask = graph.senders == first_node
@@ -227,7 +223,7 @@ def _make_last_fragment(graph, num_species):
     n_nodes = len(graph.nodes.positions)
     return _into_fragment(
         graph,
-        visited=np.arange(len(graph.nodes.positions)),
+        visited=np.arange(n_nodes),
         focus_node=0,
         target_species_probability=np.zeros((n_nodes, num_species)),
         target_node=0,
@@ -252,8 +248,10 @@ def _into_fragment(
     globals = datatypes.FragmentsGlobals(
         stop=np.array([stop], dtype=bool),  # [1]
         target_species=np.asarray([graph.nodes.species[target_node]]),  # [1]
-        target_positions=np.asarray([[pos[target_node] - pos[focus_node]]]),  # [1, 3]
-        target_positions_mask=np.array([[True]], dtype=bool),  # [1, 1]
+        target_positions=np.asarray(
+            [[pos[target_node] - pos[focus_node]]]
+        ),  # [1, 1, 3]
+        target_positions_mask=np.array([[not stop]], dtype=bool),  # [1, 1]
     )
     graph = graph._replace(nodes=nodes, globals=globals)
 

symphony/loss.py
@@ -76,10 +76,38 @@ def generation_loss(
 
     def position_loss() -> jnp.ndarray:
         """Computes the loss over position probabilities."""
+        assert graphs.globals.target_positions.shape == (num_graphs, num_targets, 3), (
+            graphs.globals.target_positions.shape,
+            num_graphs,
+            num_targets,
+            3,
+        )
         position_logits = preds.globals.radial_logits + preds.globals.angular_logits
-        assert position_logits.shape == (num_graphs, num_targets)
+        assert position_logits.shape == (num_graphs, num_targets), (
+            position_logits.shape,
+            num_graphs,
+            num_targets,
+        )
 
-        return position_logits.mean(axis=-1) * graphs.globals.target_positions_mask
+        target_positions_mask = graphs.globals.target_positions_mask
+        assert target_positions_mask.shape == (num_graphs, num_targets)
+
+        loss_position = -position_logits
+        loss_position = jnp.where(target_positions_mask, loss_position, 0)
+        loss_position = loss_position.sum(axis=-1)
+        num_valid_targets = jnp.maximum(1, target_positions_mask.sum(axis=-1))
+        loss_position /= num_valid_targets
+
+        # jax.debug.print("target_positions={x}", x=graphs.globals.target_positions)
+        # jax.debug.print("radial_logits={x}", x=preds.globals.radial_logits)
+        # jax.debug.print("angular_logits={x}", x=preds.globals.angular_logits)
+        # jax.debug.print("mask={x}", x=graphs.globals.target_positions_mask)
+        # jax.debug.print("loss_position={x}", x=loss_position)
+        # jax.debug.print("")
+
+        assert loss_position.shape == (num_graphs,)
+
+        return loss_position
 
     # If we should predict a STOP for this fragment, we do not have to predict a position.
     loss_focus_and_atom_type = focus_and_atom_type_loss()

symphony/models/angular_predictors/linear_angular_predictor.py
@@ -40,7 +40,8 @@ class LinearAngularPredictor(AngularPredictor):
         self.sampling_num_steps = sampling_num_steps
         self.sampling_init_step_size = sampling_init_step_size
 
-    def coeffs(self, radius: float, conditioning: e3nn.IrrepsArray):
+    def coeffs(self, radius: float, conditioning: e3nn.IrrepsArray) -> e3nn.IrrepsArray:
+        """Computes the spherical harmonic coefficients at the given radius."""
         radial_embed = e3nn.bessel(
             radius, self.radial_mlp_latent_size, x_max=self.max_radius
         )
@@ -60,23 +61,10 @@ class LinearAngularPredictor(AngularPredictor):
 
         return coeffs
 
-    def logits(
-        self, position: e3nn.IrrepsArray, conditioning: e3nn.IrrepsArray
-    ) -> float:
-        """Computes the logits for the given position."""
-        assert position.shape == (3,), position.shape
-
-        radius = jnp.linalg.norm(position.array)
-        coeffs = self.coeffs(radius, conditioning)
-        normalized_position = position / radius
-        return self.logits_with_coeffs(normalized_position, coeffs)
-
-    def logits_with_coeffs(
+    def unnormalized_logits(
         self, normalized_position: e3nn.IrrepsArray, coeffs: e3nn.IrrepsArray
     ) -> float:
-        """Computes the logits for the given normalized position and coefficients."""
-        assert normalized_position.shape == (3,), normalized_position.shape
-
+        """Computes the unnormalized logits for the given position."""
         vals = e3nn.to_s2point(coeffs, normalized_position)
         vals = vals.array.squeeze(-1)
         assert vals.shape == (self.num_channels,), vals.shape
@@ -85,11 +73,18 @@ class LinearAngularPredictor(AngularPredictor):
         assert logits.shape == (), logits.shape
         return logits
 
-    def log_partition_function(
-        self, radius: float, conditioning: e3nn.IrrepsArray
+    def log_prob(
+        self, position: e3nn.IrrepsArray, conditioning: e3nn.IrrepsArray
     ) -> float:
-        """Computes the log partition function at this radius."""
-        coeffs = self.coeffs(radius, conditioning)
+        """Computes the logits for the given position and coefficients."""
+        # Normalize the position.
+        normalized_position = position / jnp.linalg.norm(position.array)
+        assert normalized_position.shape == (3,), normalized_position.shape
+
+        # Compute the coefficients at this radius.
+        coeffs = self.coeffs(jnp.linalg.norm(position.array), conditioning)
+
+        # We have to compute the log partition function, because the distribution is not normalized.
         prob_signal = e3nn.to_s2grid(
             coeffs,
             res_beta=self.res_beta,
@@ -101,8 +96,8 @@ class LinearAngularPredictor(AngularPredictor):
             self.res_beta,
             self.res_alpha,
         )
-
-        prob_signal = prob_signal.replace_values(prob_signal.grid_values)
+        factor = jnp.max(prob_signal.grid_values)
+        prob_signal = prob_signal.replace_values(prob_signal.grid_values - factor)
         prob_signal = prob_signal.replace_values(jnp.exp(prob_signal.grid_values))
         prob_signal = prob_signal.replace_values(
             jnp.sum(prob_signal.grid_values, axis=-3)
@@ -112,15 +107,23 @@ class LinearAngularPredictor(AngularPredictor):
         log_Z = jnp.log(prob_signal.integrate().array.sum())
         assert log_Z.shape == (), log_Z.shape
 
-        return log_Z
+        # We can compute the logits.
+        vals = e3nn.to_s2point(coeffs, normalized_position)
+        vals -= factor
+        vals = vals.array.squeeze(-1)
+        assert vals.shape == (self.num_channels,), vals.shape
 
-    def log_prob(
-        self, position: e3nn.IrrepsArray, conditioning: e3nn.IrrepsArray
-    ) -> float:
-        """Computes the log probability of the given position."""
-        return self.logits(position, conditioning) - self.log_partition_function(
-            jnp.linalg.norm(position.array), conditioning
-        )
+        logits = jax.scipy.special.logsumexp(vals, axis=-1)
+        assert logits.shape == (), logits.shape
+
+        # jax.debug.print("grid_values_min={x}, grid_values_max={y}", x=jnp.min(prob_signal.grid_values), y=jnp.max(prob_signal.grid_values))
+        # jax.debug.print("integral={x}", x=prob_signal.integrate())
+        # jax.debug.print("normalized_position={x}", x=normalized_position)
+        # jax.debug.print("coeffs={x}", x=coeffs)
+        # jax.debug.print("vals={x}", x=vals)
+        # jax.debug.print("logits={x}", x=logits)
+
+        return logits - log_Z
 
     @staticmethod
     def coeffs_to_probability_distribution(
@@ -174,7 +177,7 @@ class LinearAngularPredictor(AngularPredictor):
 
         def score(sample: e3nn.IrrepsArray) -> float:
             """Computes the score at the given sample."""
-            return jax.grad(self.logits_with_coeffs, argnums=0)(sample, coeffs)
+            return jax.grad(self.unnormalized_logits, argnums=0)(sample, coeffs)
 
         def project_update_on_tangent_space(
             sample: e3nn.IrrepsArray, update: e3nn.IrrepsArray
@@ -205,9 +208,9 @@ class LinearAngularPredictor(AngularPredictor):
 
             # Apply Metropolis-Hastings correction.
             key, mh_key = jax.random.split(key)
-            log_acceptance_ratio = self.logits_with_coeffs(
+            log_acceptance_ratio = self.unnormalized_logits(
                 new_sample, coeffs
-            ) - self.logits_with_coeffs(sample, coeffs)
+            ) - self.unnormalized_logits(sample, coeffs)
             log_acceptance_ratio = jnp.minimum(0, log_acceptance_ratio)
             acceptance_ratio = jnp.exp(log_acceptance_ratio)
             acceptance = jax.random.bernoulli(mh_key, acceptance_ratio)

symphony/train.py
@@ -54,12 +54,23 @@ def device_batch(
 def create_optimizer(config: ml_collections.ConfigDict) -> optax.GradientTransformation:
     """Create an optimizer as specified by the config."""
     if config.optimizer == "adam":
-        return optax.chain(
-            optax.adam(learning_rate=config.learning_rate),
-            optax.clip_by_global_norm(config.gradient_clip_norm),
-        )
+        return optax.adam(learning_rate=config.learning_rate)
     elif config.optimizer == "sgd":
         return optax.sgd(learning_rate=config.learning_rate, momentum=config.momentum)
+
+
+def fill_in_target_positions(graphs: datatypes.Fragments) -> datatypes.Fragments:
+    """Fill in the target positions with non-zero values for the graphs."""
+    # Ensure that the target positions are not all zeros.
+    return graphs._replace(
+        globals=graphs.globals._replace(
+            target_positions=jnp.where(
+                jnp.all(graphs.globals.target_positions == 0.0, axis=-1)[:, None],
+                jnp.ones_like(graphs.globals.target_positions) * 1e-3,
+                graphs.globals.target_positions,
+            )
+        )
+    )
 
 
 @functools.partial(jax.pmap, axis_name="device", static_broadcasted_argnums=[3, 4, 5])
@@ -73,6 +84,9 @@ def train_step(
     noise_std: float,
 ) -> Tuple[train_state.TrainState, metrics.Collection]:
     """Performs one update step over the current batch of graphs."""
+
+    # Ensure that the target positions are not all zeros.
+    graphs = fill_in_target_positions(graphs)
 
     def loss_fn(params: optax.Params, graphs: datatypes.Fragments) -> float:
         preds = state.apply_fn(params, None, graphs)
@@ -128,6 +142,10 @@ def evaluate_step(
     loss_kwargs: Dict[str, Union[float, int]],
 ) -> metrics.Collection:
     """Computes metrics over a set of graphs."""
+
+    # Ensure that the target positions are not all zeros.
+    graphs = fill_in_target_positions(graphs)
+
     # Compute predictions and resulting loss.
     preds = state.apply_fn(state.params, rng, graphs)
     total_loss, (
@@ -286,7 +304,7 @@ def train_and_evaluate(
 
         # Evaluate model, if required.
         if step % config.eval_every_steps == 0 or first_or_last_step:
-            logging.log_first_n(logging.INFO, "Evaluating model at initialization.", 1)
+            logging.log_first_n(logging.INFO, "Evaluating model for the first time.", 1)
             rng, eval_rng = jax.random.split(rng)
             state = evaluate_model_hook(state, eval_rng)
             checkpoint_hook(state)
@@ -294,7 +312,7 @@ def train_and_evaluate(
         # Generate molecules, if required.
         if step % config.generate_every_steps == 0 or first_or_last_step:
             logging.log_first_n(
-                logging.INFO, "Generating molecules at initialization.", 1
+                logging.INFO, "Generating molecules for the first time.", 1
             )
             generate_molecules_hook(state)
 