794445e3ebf438e7296ca902dc605ae5703f3b82
Song Kim <songkimwl@gmail.com>
2024-04-26 16:49:19 -0400

    dataset utils

--- symphony/data/datasets/tmqm.py
+++ symphony/data/datasets/tmqm.py
@@ -4,12 +4,9 @@ import tqdm
 from absl import logging
 import os
 import zipfile
-import tarfile
-import urllib
+from sh import gunzip
 import urllib.error
 import urllib.request
-from git import Repo
-from sh import gunzip
 import numpy as np
 import ase
 import rdkit.Chem as Chem
@@ -86,100 +83,6 @@ class TMQMDataset(datasets.InMemoryDataset):
         }
         return permuted_indices
 
-def download_url(url: str, root: str) -> str:
-    """Download if file does not exist in root already. Returns path to file."""
-    filename = url.rpartition("/")[2]
-    file_path = os.path.join(root, filename)
-
-    try:
-        if os.path.exists(file_path):
-            logging.info(f"Using downloaded file: {file_path}")
-            return file_path
-        data = urllib.request.urlopen(url)
-    except urllib.error.URLError:
-        # No internet connection
-        if os.path.exists(file_path):
-            logging.info(f"No internet connection! Using downloaded file: {file_path}")
-            return file_path
-
-        raise ValueError(f"Could not download {url}")
-
-    chunk_size = 1024
-    total_size = int(data.info()["Content-Length"].strip())
-
-    if os.path.exists(file_path):
-        if os.path.getsize(file_path) == total_size:
-            logging.info(f"Using downloaded and verified file: {file_path}")
-            return file_path
-
-    logging.info(f"Downloading {url} to {file_path}")
-
-    with open(file_path, "wb") as f:
-        with tqdm.tqdm(total=total_size) as pbar:
-            while True:
-                chunk = data.read(chunk_size)
-                if not chunk:
-                    break
-                f.write(chunk)
-                pbar.update(chunk_size)
-
-    return file_path
-
-
-def extract_zip(path: str, root: str):
-    """Extract zip if content does not exist in root already."""
-    logging.info(f"Extracting {path} to {root}...")
-    with zipfile.ZipFile(path, "r") as f:
-        for name in f.namelist():
-            if name.endswith("/"):
-                logging.info(f"Skip directory {name}")
-                continue
-            out_path = os.path.join(root, name)
-            file_size = f.getinfo(name).file_size
-            if os.path.exists(out_path) and os.path.getsize(out_path) == file_size:
-                logging.info(f"Skip existing file {name}")
-                continue
-            logging.info(f"Extracting {name} to {root}...")
-            f.extract(name, root)
-
-
-def clone_url(url: str, root: str) -> str:
-    """Clone if repo does not exist in root already. Returns path to repo."""
-    repo_path = os.path.join(root, url.rpartition("/")[-1].rpartition(".")[0])
-
-    if os.path.exists(repo_path):
-        logging.info(f"Using cloned repo: {repo_path}")
-        return repo_path
-
-    logging.info(f"Cloning {url} to {repo_path}")
-    Repo.clone_from(url, repo_path)
-
-    return repo_path
-
-
-def extract_zip(path: str, root: str):
-    """Extract zip if content does not exist in root already."""
-    logging.info(f"Extracting {path} to {root}...")
-    with zipfile.ZipFile(path, "r") as f:
-        for name in f.namelist():
-            if name.endswith("/"):
-                logging.info(f"Skip directory {name}")
-                continue
-            out_path = os.path.join(root, name)
-            file_size = f.getinfo(name).file_size
-            if os.path.exists(out_path) and os.path.getsize(out_path) == file_size:
-                logging.info(f"Skip existing file {name}")
-                continue
-            logging.info(f"Extracting {name} to {root}...")
-            f.extract(name, root)
-
-
-def extract_tar(path: str, root: str):
-    """Extract tar."""
-    logging.info(f"Extracting {path} to {root}...")
-    with tarfile.TarFile(path, "r") as f:
-        f.extractall(path=root)
-
 
 def load_tmqm(root_dir: str) -> List[ase.Atoms]:
     """Load the TMQM dataset."""
@@ -192,7 +95,7 @@ def load_tmqm(root_dir: str) -> List[ase.Atoms]:
         if not os.path.exists(root_dir):
             os.makedirs(root_dir)
         logging.info(f"Cloning TMQM repository to {root_dir}...")
-        _ = clone_url(TMQM_URL, root_dir)
+        _ = datasets.utils.clone_url(TMQM_URL, root_dir)
         if not os.path.exists(xyzs_path):
             os.makedirs(xyzs_path)
 
