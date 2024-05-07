064be764116bac8faf848e3c6b15dc5def20b61e
Song Kim <songkimwl@gmail.com>
2024-05-07 16:27:22 -0400

    conditional generation

--- analyses/conditional_generation.py
+++ analyses/conditional_generation.py
@@ -19,16 +19,15 @@ def get_fragment_list(mols: Sequence[ase.Atoms], num_mols: int):
     fragments = []
     for i in range(num_mols):
         mol = mols[i]
-        num_atoms = len(mol)
-        for j in range(num_atoms):
-            if mol.numbers[j] == 1:
-                fragment = ase.Atoms(
-                    positions=np.vstack([mol.positions[:j], mol.positions[j + 1 :]]),
-                    numbers=np.concatenate([mol.numbers[:j], mol.numbers[j + 1 :]]),
-                    cell=mol.cell,
-                    pbc=True
-                )
-                fragments.append(fragment)
+        filter_ON = (mol.numbers == 8) | (mol.numbers == 7)
+        fragment = ase.Atoms(
+            # all perovskites in perov5 contain either O or N as negative ion
+            positions=mol.positions[~filter_ON, :],
+            numbers=mol.numbers[~filter_ON],
+            cell=mol.cell,
+            pbc=True
+        )
+        fragments.append(fragment)
     return fragments
 
 
@@ -38,9 +37,15 @@ def main(unused_argv: Sequence[str]):
     step = flags.FLAGS.step
     num_seeds_per_chunk = 1
     max_num_atoms = 200
-    max_num_steps = 10
     num_mols = 20
-    outputdir = "conditional_generation"
+
+    atomic_numbers = np.array([
+            3, 4, 5, 7, 8, 9, 11, 12, 13, 14, 16, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
+            32, 33, 37, 38, 39, 40, 41, 42, 44, 45, 46, 
+            47, 48, 49, 50, 51, 52, 55, 56, 57, 72, 73, 
+            74, 75, 76, 77, 78, 79, 80, 81, 82, 83
+        ])
 
     all_mols = perov.load_perov(get_root_dir('perov5'), supercell=False)
     mols_by_split = {"train": all_mols['train'][:num_mols], "test": all_mols['test'][-num_mols:]}
@@ -53,17 +58,19 @@ def main(unused_argv: Sequence[str]):
         ]
         print(f"Number of fragments for {split}: {len(mol_list)}")
 
-        gen_mol_list = generate_molecules.generate_molecules(
+        gen_mol_list = generate_molecules.generate_molecules_from_workdir(
             flags.FLAGS.workdir,
             os.path.join(flags.FLAGS.outputdir, split),
             beta_species,
             beta_position,
             step,
+            flags.FLAGS.steps_for_weight_averaging,
             len(mol_list),
             num_seeds_per_chunk,
             mol_list,
             max_num_atoms,
-            max_num_steps,
+            flags.FLAGS.avg_neighbors_per_atom,
+            atomic_numbers,
             flags.FLAGS.visualize,
         )
 
@@ -88,5 +95,15 @@ if __name__ == "__main__":
         "step",
         "best",
         "Step number to load model from. The default corresponds to the best model.",
+    )
+    flags.DEFINE_list(
+        "steps_for_weight_averaging",
+        None,
+        "Steps to average parameters over. If None, the model at the given step is used.",
+    )
+    flags.DEFINE_integer(
+        "avg_neighbors_per_atom",
+        10,
+        "Average number of neighbors per atom.",
     )
     app.run(main)
