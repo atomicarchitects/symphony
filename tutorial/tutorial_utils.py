6b0792e0e9695d3e79feb9db3d505aef5325f0a4
Ameya Daigavane <ameya.d.98@gmail.com>
2024-07-18 12:47:27 -0700

    Model inference working, need to do visualizations.

--- tutorial/tutorial_utils.py
+++ tutorial/tutorial_utils.py
@@ -10,6 +10,34 @@ import ml_collections
 
 from symphony import models
 
+from symphony import datatypes
+import ase
+import matscipy.neighbours
+import numpy as np
+import jraph
+
+
+def ase_atoms_to_jraph_graph(
+    atoms: ase.Atoms, atomic_numbers: jnp.ndarray, nn_cutoff: float
+) -> jraph.GraphsTuple:
+    # Create edges
+    receivers, senders = matscipy.neighbours.neighbour_list(
+        quantities="ij", positions=atoms.positions, cutoff=nn_cutoff, cell=np.eye(3)
+    )
+
+    # Get the species indices
+    species = np.searchsorted(atomic_numbers, atoms.numbers)
+
+    return jraph.GraphsTuple(
+        nodes=datatypes.NodesInfo(np.asarray(atoms.positions), np.asarray(species)),
+        edges=np.ones(len(senders)),
+        globals=None,
+        senders=np.asarray(senders),
+        receivers=np.asarray(receivers),
+        n_node=np.array([len(atoms)]),
+        n_edge=np.array([len(senders)]),
+    )
+
 
 def load_model_at_step(
     workdir: str, step: str, run_in_evaluation_mode: bool,
