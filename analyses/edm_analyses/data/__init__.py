8fc0e8deb1cdb9d1fe21cc72cce28519fbcac80c
Ameya Daigavane <ameya.d.98@gmail.com>
2024-07-18 18:55:16 -0700

    Remove big analyses folder.

--- analyses/edm_analyses/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from analyses.edm_analyses.data.utils import initialize_datasets
-from analyses.edm_analyses.data.collate import PreprocessQM9
-from analyses.edm_analyses.data.dataset_class import ProcessedDataset
--- analyses/edm_analyses/data/prepare/__init__.py
+++
@@ -1,4 +0,0 @@
-from analyses.edm_analyses.data.prepare.download import *
-from analyses.edm_analyses.data.prepare.process import *
-from analyses.edm_analyses.data.prepare.qm9 import *
-from analyses.edm_analyses.data.prepare.md17 import *
